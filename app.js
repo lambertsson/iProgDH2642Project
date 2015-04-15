7b86aeb4422cb3dbb3006bce50772f1aaeffd430
edvinmoks <edvin.moks@gmail.com>
2015-04-15 12:56:46 +0200

    Starting files
    Project idea is prototype

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	//window.stage = "starter";
+	//window.stage = "main";
+	//window.stage = "dessert";
+	//createTestData()
+	/*
+	var model = new DinnerModel();
+	//model.addDishToMenu(1);
+	//model.addDishToMenu(100);
+	//model.addDishToMenu(201);
+
+    var sidebarView = new SidebarView($("#sidebarView"),model);
+	var sidebarViewController = new SidebarViewController(sidebarView,model);	
+	*/
+	
+
+	
+
+	
+
+
+
+});
