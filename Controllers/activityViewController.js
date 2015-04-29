2c8532b94851772cb6f658af6c21ab2f40e60856
yuluoluo <liuyechuanzi@gmail.com>
2015-04-29 21:21:25 +0200

    add popup window

--- Controllers/activityViewController.js
+++ Controllers/activityViewController.js
@@ -1,8 +1,7 @@
 var ActivityViewController = function(view, model ) {
     $("#addActivityBtn").on('click', function(){
         
-		$("#activityView").hide();
-        $("#addActivityView").show();
+		$("#addActivityView").fadeIn(300);
 
         // ***************************
         // Temporary, just for testing.
